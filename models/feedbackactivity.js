bab405671babcc54e2c8c29714ce06c29dbff9a1
Johanes Raphael Nandaputra <johanesraphaeln@gmail.com>
2024-01-17 00:55:02 +0700

    Create FeedbackActivities migration and model

---
+++ models/feedbackactivity.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class FeedbackActivity extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  FeedbackActivity.init({
+    FeedbackId: DataTypes.INTEGER,
+    ActivityId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'FeedbackActivity',
+  });
+  return FeedbackActivity;
+};
