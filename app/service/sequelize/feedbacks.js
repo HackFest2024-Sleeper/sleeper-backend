52ef77fc65ea1fe5926733dee20eb659c4324e8c
Johanes Raphael Nandaputra <johanesraphaeln@gmail.com>
2024-01-17 01:02:47 +0700

    Adjust feedback logic endpoint and request body

--- app/service/sequelize/feedbacks.js
+++ app/service/sequelize/feedbacks.js
@@ -2,9 +2,12 @@ const { NotFoundError, BadRequestError } = require('../../errors');
 
 const User = require('../../../models').User;
 const Feedback = require('../../../models').Feedback;
+const Activity = require('../../../models').Activity;
+const FeedbackActivity = require('../../../models').FeedbackActivity;
 
 const getAllFeedbacksUser = async (req) => {
-  const { uid } = req.user;
+  // const { uid } = req.user;
+  const uid = 'S5Kkhk64eEY4zgWRoKiB2uZsMw72';
   const { page = 1, limit = 10 } = req.query;
   const whereClause = {};
 
@@ -16,13 +19,19 @@ const getAllFeedbacksUser = async (req) => {
   const user = await User.findOne({ where: { uid } });
   if (!user) {
     throw new NotFoundError(`User with uid ${uid} was not found`);
+  } else {
+    whereClause.UserId = user.id;
   }
 
   const result = await Feedback.findAndCountAll({
     limit,
     offset: (page - 1) * limit,
     where: whereClause,
-    attributes: ['id', 'foods', 'exercises', 'feedbacks', 'date'],
+    attributes: ['id', 'feedbacks', 'date'],
+    include: {
+      model: Activity,
+      attributes: ['id'],
+    },
   });
 
   return {
@@ -33,7 +42,8 @@ const getAllFeedbacksUser = async (req) => {
 };
 
 const getOneFeedbackUser = async (req) => {
-  const { uid } = req.user;
+  // const { uid } = req.user;
+  const uid = 'S5Kkhk64eEY4zgWRoKiB2uZsMw72';
 
   const user = await User.findOne({ where: { uid } });
   if (!user) {
@@ -41,27 +51,48 @@ const getOneFeedbackUser = async (req) => {
   }
 
   const result = await Feedback.findOne({
-    attributes: ['id', 'foods', 'exercises', 'feedbacks', 'date'],
+    where: { UserId: user.id },
+    attributes: ['id', 'feedbacks', 'date'],
+    include: {
+      model: Activity,
+      attributes: ['id'],
+    },
   });
 
   return result;
 };
 
 const inputDailyFeedbacksUser = async (req) => {
-  const { exercises, foods, feedbacks } = req.body;
-  const { uid } = req.user;
+  const { activityIds, feedbacks, date } = req.body;
+  // const { uid } = req.user;
+  const uid = 'S5Kkhk64eEY4zgWRoKiB2uZsMw72';
+  let activities = null;
 
   const user = await User.findOne({ where: { uid } });
   if (!user) {
     throw new NotFoundError(`User with uid ${uid} was not found`);
   }
 
+  if (activityIds) {
+    activities = await Activity.findAll({
+      where: {
+        id: activityIds,
+        UserId: user.id,
+      },
+    });
+
+    const foundIds = activities.map((activity) => activity.id);
+    const missingIds = activityIds.filter((id) => !foundIds.includes(id));
+
+    if (missingIds.length > 0) {
+      throw new NotFoundError(`Activities with id [${missingIds}] not found.`);
+    }
+  }
+
   const [feedback, created] = await Feedback.findOrCreate({
-    where: { date: new Date(), UserId: user.id },
+    where: { date: date, UserId: user.id },
     defaults: {
-      foods: foods,
-      exercises: exercises,
-      feedbacks: feedbacks,
+      feedback: feedbacks,
     },
   });
 
@@ -70,6 +101,13 @@ const inputDailyFeedbacksUser = async (req) => {
       `Daily feedback already filled for ${feedback.date}`
     );
   }
+
+  await FeedbackActivity.bulkCreate(
+    activities.map((activity) => ({
+      FeedbackId: feedback.id,
+      ActivityId: activity.id,
+    }))
+  );
 
   return feedback;
 };
